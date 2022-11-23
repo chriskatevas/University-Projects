4d1fbdd3a8dce25f3339441862b950ba826860cc
Chris Katevas <118923706+chriskatevas@users.noreply.github.com>
2022-11-23 20:30:47 +0200

    Update newrank.c

--- University Projects/DiscreteMaths/newrank.c
+++ University Projects/DiscreteMaths/newrank.c
@@ -1,10 +1,4 @@
 /*
-    Main Author: Katevas Chris
-    p18068
-    github: https://github.com/katevaschris
-    
-    Athens 15, May 2019
-    
     Main purpose: Input String and new position and gonna print the new lexicography word
                     in givven rank
 */
